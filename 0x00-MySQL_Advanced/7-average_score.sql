b953637d4c647d1eb2f08f965658a3ee823cbe63
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-12 00:04:52 +0200

    Commit

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,14 @@
+-- Script creates stored procedure ComputeAverageScoreForUser
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN user_id INT
+)
+BEGIN
+	DECLARE average_score FLOAT;
+
+	SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
+	UPDATE users SET average_score = average_score WHERE id=user_id;
+END
+//
+DELIMITER ;
