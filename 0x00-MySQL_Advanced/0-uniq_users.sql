6c4b9f39676abcc5e20f2d3eb73e5f540859957d
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-11 19:03:20 +0200

    Commit

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
--- Create table users script
 -- Columns (id (int, auto increment, not null), email(varchar(255)), name(varchar(255))
 CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	email VARCHAR(255) UNIQUE NOT NULL,
-	name VARCHAR(255),
+	name VARCHAR(255)
 );
