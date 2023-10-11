58f0c604e16a294dacee562906d5fa0e36180d46
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-12 00:06:13 +0200

    Commit

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -9,6 +9,6 @@ BEGIN
 
 	SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
 	UPDATE users SET average_score = average_score WHERE id=user_id;
-END
+END;
 //
 DELIMITER ;
