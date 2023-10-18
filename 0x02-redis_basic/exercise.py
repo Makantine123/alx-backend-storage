e6b94e7c7b18a457c89b0c76f0729070477fc8a8
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-18 23:01:17 +0200

    commit

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module contains Cache class
+"""
+
+
+from re import U
+import redis
+import uuid
+
+
+class Cache:
+    """
+    Cache class, stores an instance of the Redis client
+    """
+
+    def __init__(self) -> None:
+        """Initialisation of class"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str, bytes, int, float) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
