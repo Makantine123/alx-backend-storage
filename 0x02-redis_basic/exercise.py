f671963868a4813eded447fd6d2fe02890191019
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-18 23:07:49 +0200

    Commit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,6 +17,7 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: str, bytes, int, float) -> str:
+        """Store method"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
