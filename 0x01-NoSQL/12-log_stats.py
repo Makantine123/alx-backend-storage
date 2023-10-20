06f585ce45565726ea24fcb0d2a51175f9467ceb
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-20 11:26:43 +0200

    Commit

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,22 +9,20 @@ client = MongoClient('mongodb://localhost:27017')
 db = client['logs']
 collection = db['nginx']
 
-# Count the total number of documents in the collection
 total_logs = collection.count_documents({})
 
-# Count the number of documents with each method
 methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_counts = {method: collection.count_documents({"method": method}) for method in methods}
+method_counts = {
+    method: collection.count_documents({
+        "method": method}) for method in methods}
 
-# Count the number of documents with method=GET and path=/status
-status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+status_check_count = collection.count_documents({
+    "method": "GET", "path": "/status"})
 
-# Display the results
 print(f"{total_logs} logs")
 print("Methods:")
 for method, count in method_counts.items():
     print(f"\tmethod {method}: {count}")
 print(f"{status_check_count} status check")
 
-# Close the MongoDB connection
 client.close()
