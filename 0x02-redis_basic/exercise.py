a3ed9fb60aea15b21716f82931223fa2293cc356
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-20 13:49:48 +0200

    Commit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Module contains Cache class
 """
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Union
 
 
 class Cache:
@@ -22,3 +22,16 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Callable = None):
+        """Retrieves a value from Redis data storage"""
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> Union[str, bytes]:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, bytes]:
+        return self.get(key, fn=int)
