6296c04d69c6373e611ae3f6608ddb5d635d190f
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-20 11:13:37 +0200

    Commit

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,7 +6,6 @@ Modules
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    """ Provides some stats about Nginx logs stored in MongoDB """
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
